5f199e5d549b42ed0ab56013894929ed01b71561
Shivesh Sinha <realshivesh@gmail.com>
2022-03-20 14:56:23 +0530

    installs axios and makes fetch api for news & jobs

---
+++ api/fetchNews.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+//fetch news from hacker news api
+//url: https://hacker-news.firebaseio.com/v0/topstories.json
+
+const fetchNews = () => {
+  return axios
+    .get('https://hacker-news.firebaseio.com/v0/topstories.json')
+    .then(response => {
+      response.data;
+      console.log(response.data);
+    })
+    .catch(error => console.log(error));
+};
+//this will return an array of ids
+
+export default fetchNews;
