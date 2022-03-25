f0070ea07690f587a8cb5895cb344ad77771ff16
Shivesh Sinha <realshivesh@gmail.com>
2022-03-25 08:50:30 +0530

    makes changes in fetch files

--- api/fetchJobs.js
+++ api/fetchJobs.js
@@ -3,15 +3,16 @@ import axios from 'axios';
 //fetch news from hacker news api
 //url: https://hacker-news.firebaseio.com/v0/topstories.json
 
-const fetchNews = () => {
+const fetchJobs = setJobId => {
   return axios
     .get('https://hacker-news.firebaseio.com/v0/jobstories.json')
     .then(response => {
       response.data;
-      console.log(response.data);
+      console.log(response.data.slice(0, 10));
+      setJobId(response.data.slice(0, 10));
     })
     .catch(error => console.log(error));
 };
 //this will return an array of ids
 
-export default fetchNews;
+export default fetchJobs;
--- api/fetchNews.js
+++ api/fetchNews.js
@@ -3,12 +3,13 @@ import axios from 'axios';
 //fetch news from hacker news api
 //url: https://hacker-news.firebaseio.com/v0/topstories.json
 
-const fetchNews = () => {
+const fetchNews = setNewsId => {
   return axios
     .get('https://hacker-news.firebaseio.com/v0/topstories.json')
     .then(response => {
-      response.data;
-      console.log(response.data);
+      response.data.slice(0, 10);
+      console.log(response.data.slice(0, 10));
+      setNewsId(response.data.slice(0, 10));
     })
     .catch(error => console.log(error));
 };
